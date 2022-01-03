68c8e34816d51d983f84fb98bc502201c3a7e1be
James Phan <nhat_huy_phan@sfu.ca>
2022-01-03 02:30:16 -0800

    Created Navbar

---
+++ components/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+  return (
+    <nav>
+      <a>h3</a>
+      <a>About</a>
+      <a>Projects</a>
+      <a>Work Experience</a>
+      <a>Contact</a>
+    </nav>
+  );
+};
+
+export default Navbar;
