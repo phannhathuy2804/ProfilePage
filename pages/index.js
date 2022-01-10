f4f1fb47bb52c523d4a7b58827e8ce4d7e82f710
James Phan <nhat_huy_phan@sfu.ca>
2022-01-10 03:29:22 -0800

    Changed navbar stylesheet

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
 import Head from "next/head";
+import Banner from "../components/Banner";
 import Navbar from "../components/Navbar";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div>
-      <Navbar />
+      <Banner />
     </div>
   );
 }
