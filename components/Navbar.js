73a8c51f7c781ca78afdb0ef63b1db0f88d7bec1
James Phan <nhat_huy_phan@sfu.ca>
2022-01-10 01:18:31 -0800

    Add effect for navbar buttons

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,6 +1,9 @@
 const Navbar = () => {
   return (
     <div className="navbar">
+      <div className="logo">
+        <h3>James Phan</h3>
+      </div>
       <nav className="nav">
         <a>Home</a>
         <a>About</a>
