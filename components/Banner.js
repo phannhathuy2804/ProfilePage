9b718487b15530c81f8de9fa241f4c2e8a924832
James Phan <nhat_huy_phan@sfu.ca>
2022-01-10 03:27:01 -0800

    Created Banner component

---
+++ components/Banner.js
@@ -0,0 +1,13 @@
+const Banner = () => {
+  return (
+    <div className="banner">
+      <div className="content">
+        <h2 className="title welcome">Welcome to my portfolio</h2>
+        <h2 className="title greeting">My name is</h2>
+        <h1 className="title name">James Phan</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
