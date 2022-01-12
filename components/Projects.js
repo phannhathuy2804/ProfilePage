9440a6439f275325069a976060117c414d7e270e
James Phan <nhat_huy_phan@sfu.ca>
2022-01-12 02:34:35 -0800

    Create projects component

---
+++ components/Projects.js
@@ -0,0 +1,11 @@
+const Projects = () => {
+  return (
+    <div className="projects page">
+      <div className="content">
+        <h1 className="title">Projects</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Projects;
