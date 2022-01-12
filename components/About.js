901eb55047bf14ace29cb0316a36a1b025c19f3a
James Phan <nhat_huy_phan@sfu.ca>
2022-01-12 02:02:06 -0800

    Modify stylesheet of About component

--- components/About.js
+++ components/About.js
@@ -1,37 +1,52 @@
 const About = () => {
   return (
-    <div className="about">
+    <div className="about page">
       <div className="content">
         <div className="container ">
           <h1 className="title">About Me</h1>
           <div className="description">
-            asdsadasdddsk sdsdsd sdsdsdsd sdsdsdsdsd sdsdsd sd /n asass Building
-            state-of-the-art, easy-to-use, user-friendly websites and
-            applications is truly a passion of mine and I am confident I would
-            be an excellent addition to any organization. In addition to my
-            knowledge base, I actively seek out new technologies and stay
-            up-to-date on industry trends and advancements. This has allowed me
-            to stay ahead of the curve and deliver exceptional work to all of my
-            employers. Building state-of-the-art, easy-to-use, user-friendly
-            websites and applications is truly a passion of mine and I am
-            confident I would be an excellent addition to any organization. In
-            addition to my knowledge base, I actively seek out new technologies
-            and stay up-to-date on industry trends and advancements. This has
-            allowed me to stay ahead of the curve and deliver exceptional work
-            to all of my employers.Building state-of-the-art, easy-to-use,
-            user-friendly websites and applications is truly a passion of mine
-            and I am confident I would be an excellent addition to any
-            organization. In addition to my knowledge base, I actively seek out
-            new technologies and stay up-to-date on industry trends and
-            advancements. This has allowed me to stay ahead of the curve and
-            deliver exceptional work to all of my employers.Building
-            state-of-the-art, easy-to-use, user-friendly websites and
-            applications is truly a passion of mine and I am confident I would
-            be an excellent addition to any organization. In addition to my
-            knowledge base, I actively seek out new technologies and stay
-            up-to-date on industry trends and advancements. This has allowed me
-            to stay ahead of the curve and deliver exceptional work to all of my
-            employers.v
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Pellentesque pulvinar pellentesque habitant morbi tristique
+            senectus. Amet cursus sit amet dictum sit. Congue eu consequat ac
+            felis donec. Pellentesque sit amet porttitor eget dolor morbi non.
+            Laoreet non curabitur gravida arcu ac tortor. Diam vel quam
+            elementum pulvinar etiam non quam. Tincidunt dui ut ornare lectus.
+            Condimentum vitae sapien pellentesque habitant morbi tristique
+            senectus et. Rhoncus mattis rhoncus urna neque viverra justo nec. A
+            arcu cursus vitae congue mauris rhoncus aenean vel. Dignissim
+            suspendisse in est ante in nibh mauris cursus mattis. Nisi est sit
+            amet facilisis magna. Enim blandit volutpat maecenas volutpat
+            blandit aliquam etiam erat velit. Fermentum iaculis eu non diam
+            phasellus vestibulum. Bibendum arcu vitae elementum curabitur vitae
+            nunc. Dictum fusce ut placerat orci nulla pellentesque dignissim. Mi
+            quis hendrerit dolor magna eget est lorem ipsum. Elementum nisi quis
+            eleifend quam adipiscing vitae proin. Eu scelerisque felis imperdiet
+            proin fermentum. Tellus in hac habitasse platea dictumst. Sagittis
+            eu volutpat odio facilisis mauris sit amet massa. Senectus et netus
+            et malesuada fames ac turpis egestas. Dolor morbi non arcu risus
+            quis varius quam quisque. Vitae elementum curabitur vitae nunc sed
+            velit. Vulputate ut pharetra sit amet aliquam id diam. Turpis
+            egestas pretium aenean pharetra magna ac placerat vestibulum lectus.
+            Pulvinar elementum integer enim neque. Laoreet non curabitur gravida
+            arcu ac tortor dignissim convallis aenean. Bibendum enim facilisis
+            gravida neque convallis. Molestie a iaculis at erat pellentesque.
+            Consectetur a erat nam at lectus urna duis convallis convallis. Cum
+            sociis natoque penatibus et magnis dis. Ultrices sagittis orci a
+            scelerisque purus semper eget. Est sit amet facilisis magna etiam
+            tempor orci. Velit scelerisque in dictum non. Proin sagittis nisl
+            rhoncus mattis rhoncus urna neque viverra. Metus aliquam eleifend mi
+            in nulla posuere sollicitudin aliquam. Id faucibus nisl tincidunt
+            eget nullam non. Enim ut tellus elementum sagittis vitae et leo duis
+            ut. In ornare quam viverra orci sagittis eu volutpat odio. Dictum
+            fusce ut placerat orci nulla pellentesque dignissim enim sit. Mattis
+            enim ut tellus elementum sagittis. Gravida neque convallis a cras
+            semper auctor neque. Tellus in hac habitasse platea dictumst
+            vestibulum rhoncus est. Elementum sagittis vitae et leo. Et ultrices
+            neque ornare aenean euismod elementum nisi quis. Est pellentesque
+            elit ullamcorper dignissim cras tincidunt. Sit amet tellus cras
+            adipiscing enim eu turpis egestas. Nunc consequat interdum varius
+            sit amet mattis vulputate enim.
           </div>
         </div>
       </div>
