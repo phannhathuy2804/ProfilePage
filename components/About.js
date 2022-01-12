07a8a81147edc3d8188539508dcf753de15b67e0
James Phan <nhat_huy_phan@sfu.ca>
2022-01-11 22:56:22 -0800

    Add About component

---
+++ components/About.js
@@ -0,0 +1,42 @@
+const About = () => {
+  return (
+    <div className="about">
+      <div className="content">
+        <div className="container ">
+          <h1 className="title">About Me</h1>
+          <div className="description">
+            asdsadasdddsk sdsdsd sdsdsdsd sdsdsdsdsd sdsdsd sd /n asass Building
+            state-of-the-art, easy-to-use, user-friendly websites and
+            applications is truly a passion of mine and I am confident I would
+            be an excellent addition to any organization. In addition to my
+            knowledge base, I actively seek out new technologies and stay
+            up-to-date on industry trends and advancements. This has allowed me
+            to stay ahead of the curve and deliver exceptional work to all of my
+            employers. Building state-of-the-art, easy-to-use, user-friendly
+            websites and applications is truly a passion of mine and I am
+            confident I would be an excellent addition to any organization. In
+            addition to my knowledge base, I actively seek out new technologies
+            and stay up-to-date on industry trends and advancements. This has
+            allowed me to stay ahead of the curve and deliver exceptional work
+            to all of my employers.Building state-of-the-art, easy-to-use,
+            user-friendly websites and applications is truly a passion of mine
+            and I am confident I would be an excellent addition to any
+            organization. In addition to my knowledge base, I actively seek out
+            new technologies and stay up-to-date on industry trends and
+            advancements. This has allowed me to stay ahead of the curve and
+            deliver exceptional work to all of my employers.Building
+            state-of-the-art, easy-to-use, user-friendly websites and
+            applications is truly a passion of mine and I am confident I would
+            be an excellent addition to any organization. In addition to my
+            knowledge base, I actively seek out new technologies and stay
+            up-to-date on industry trends and advancements. This has allowed me
+            to stay ahead of the curve and deliver exceptional work to all of my
+            employers.v
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
